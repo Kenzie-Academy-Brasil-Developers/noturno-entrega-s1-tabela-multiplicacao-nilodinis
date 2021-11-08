2e3cb5bdedaba588668db9b9b493c0211a88c799
Nilo Dinis <nilodinis@hotmail.com>
2021-11-08 17:54:23 -0400

    Corrigido

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,11 +1,10 @@
 function multiplication(number) { 
    let myTable=[]
    for (let indicator=0; indicator <=number; indicator+=1){//Primeiro loop
-       myTable[indicator]=[]
+       myTable.push([])
        for(let counter=0; counter<= number; counter+=1){//Segundo loop
-           myTable.push(indicator*counter)  }//Alimentar tabela
+           myTable[indicator].push(indicator*counter)  //Alimentar tabela
+        }
     }
-   
-       console.table(myTable)
-}
-multiplication()
+        console.table(myTable)
+ }
